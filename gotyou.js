c0053d32781c78d9810121cf2e21a9c198e0c164
arealxd <metorikz@gmail.com>
2023-03-03 03:13:25 +0600

    commit with love

---
+++ gotyou.js
@@ -0,0 +1,22 @@
+function runAway() {
+  var noButton = document.getElementById("no-button")
+  var x = 0
+  var y = 0
+  var xSpeed = 5
+  var ySpeed = 3
+  setInterval(function () {
+    x += xSpeed
+    y += ySpeed
+    noButton.style.left = x + "px"
+    noButton.style.top = y + "px"
+    if (x + noButton.clientWidth > window.innerWidth || x < 0) {
+      xSpeed = -xSpeed
+    }
+    if (y + noButton.clientHeight > window.innerHeight || y < 0) {
+      ySpeed = -ySpeed
+    }
+  }, 10)
+}
+setTimeout(function () {
+  alert("ТВОЙ ЕДИНСТВЕННЫЙ ВЫБОР - ЭТО СКАЗАТЬ ДА 🥰")
+}, 5000)
